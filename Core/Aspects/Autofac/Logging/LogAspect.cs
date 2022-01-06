cec68ba87771a94c77c244204dc234e5e295f346
emir57 <emir.gurbuz06@hotmail.com>
2022-01-06 23:41:00 +0300

    LogAspect

---
+++ Core/Aspects/Autofac/Logging/LogAspect.cs
@@ -0,0 +1,50 @@
+﻿using Castle.DynamicProxy;
+using Core.CrossCuttingConcerns.Logging;
+using Core.CrossCuttingConcerns.Logging.Log4Net;
+using Core.Utilities.Interceptors;
+using Core.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Aspects.Autofac.Logging
+{
+    public class LogAspect:MethodInterception
+    {
+        private LoggerServiceBase _loggerServiceBase;
+        public LogAspect(Type loggingType)
+        {
+            if (!typeof(LoggerServiceBase).IsAssignableFrom(loggingType))
+            {
+                throw new Exception(AspectMessages.WrongLoggingType);
+            }
+            _loggerServiceBase = (LoggerServiceBase)Activator.CreateInstance(loggingType);
+        }
+        protected override void OnBefore(IInvocation invocation)
+        {
+            _loggerServiceBase.Info(GetLogDetail(invocation));
+        }
+
+        private LogDetail GetLogDetail(IInvocation invocation)
+        {
+            var logParameters = new List<LogParameter>();
+            for (int i = 0; i < invocation.Arguments.Length; i++)
+            {
+                logParameters.Add(new LogParameter
+                {
+                    Name = invocation.GetConcreteMethod().GetParameters()[i].Name,
+                    Type = invocation.Arguments[i].GetType().ToString(),
+                    Value = invocation.Arguments[i]
+                });
+            }
+            var logDetail = new LogDetail
+            {
+                MethodName = invocation.Method.Name,
+                LogParameters = logParameters
+            };
+            return logDetail;
+        }
+    }
+}
--- Core/Utilities/Messages/AspectMessages.cs
+++ Core/Utilities/Messages/AspectMessages.cs
@@ -9,5 +9,7 @@ namespace Core.Utilities.Messages
     public static class AspectMessages
     {
         public static string WrongValidationType = "Validasyon için geçersiz tip";
+
+        public static string WrongLoggingType = "Loglama için geçersiz tip";
     }
 }
