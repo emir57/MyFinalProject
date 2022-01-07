ec3deebabb1c03a61f17ea0f0001f0712f7889ba
emir57 <emir.gurbuz06@hotmail.com>
2022-01-07 15:14:43 +0300

    ExceptionLogAspect

---
+++ Core/Aspects/Autofac/Exception/ExceptionLogAspect.cs
@@ -0,0 +1,30 @@
+﻿using Castle.DynamicProxy;
+using Core.CrossCuttingConcerns.Logging.Log4Net;
+using Core.Utilities.Interceptors;
+using Core.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Aspects.Autofac.Exception
+{
+    public class ExceptionLogAspect:MethodInterception
+    {
+        private LoggerServiceBase _loggerServiceBase;
+        public ExceptionLogAspect(Type loggerService)
+        {
+            if (!typeof(LoggerServiceBase).IsAssignableFrom(loggerService))
+            {
+                throw new System.Exception(AspectMessages.WrongLoggingType);
+            }
+            _loggerServiceBase = (LoggerServiceBase)Activator.CreateInstance(loggerService);
+        }
+        protected override void OnException(IInvocation invocation, System.Exception exception)
+        {
+            var logDetailWithException = GetLogDetail(invocation);
+
+        }
+    }
+}
