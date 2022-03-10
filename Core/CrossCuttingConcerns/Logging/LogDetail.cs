223a420b6c4d06d5d14aedc24b226a6e1167d5cf
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-10 16:55:42 +0300

    LogDetail => UserEmail and UserRoles

--- Core/CrossCuttingConcerns/Logging/LogDetail.cs
+++ Core/CrossCuttingConcerns/Logging/LogDetail.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core.Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,5 +11,7 @@ namespace Core.CrossCuttingConcerns.Logging
     {
         public string MethodName { get; set; }
         public List<LogParameter> LogParameters { get; set; }
+        public string UserEmail { get; set; }
+        public List<OperationClaim> UserRoles { get; set; }
     }
 }
