00d5df47d9f6a847e1c7dd733a58286786e457cb
emir57 <emir.gurbuz06@hotmail.com>
2022-07-08 18:56:42 +0300

    Create RedisCacheManager

---
+++ Core/CrossCuttingConcerns/Caching/Redis/RedisCacheManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CrossCuttingConcerns.Caching.Redis
+{
+    public class RedisCacheManager : ICacheManager
+    {
+        private readonly RedisServer _redisServer;
+
+        public RedisCacheManager(RedisServer redisServer)
+        {
+            _redisServer = redisServer;
+        }
+
+        public void Add(string key, object value, int duration)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get<T>(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Get(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsAdd(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveByPattern(string pattern)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
