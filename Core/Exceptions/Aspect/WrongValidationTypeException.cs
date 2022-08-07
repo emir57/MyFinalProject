048f40df5b056449712c088562dd29e7cb5a22d8
emir57 <emir.gurbuz06@hotmail.com>
2022-08-07 14:16:01 +0300

    Create WrongValidationTypeException

---
+++ Core/Exceptions/Aspect/WrongValidationTypeException.cs
@@ -0,0 +1,42 @@
+﻿using Core.Utilities.Messages;
+using System;
+
+namespace Core.Exceptions.Aspect
+{
+    public class WrongValidationTypeException : Exception
+    {
+        public WrongValidationTypeException() : base(AspectMessages.WrongValidationType)
+        {
+        }
+
+        public WrongValidationTypeException(string message) : base(message)
+        {
+        }
+
+        public WrongValidationTypeException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        public static void IfNotEqual(Type obja, Type objb)
+        {
+            ifNotEqual(obja, objb);
+        }
+        public static void IfNotEqual(Type obja, object objb)
+        {
+            ifNotEqual(obja, objb.GetType());
+        }
+        public static void IfNotEqual(object obja, Type objb)
+        {
+            ifNotEqual(obja.GetType(), objb);
+        }
+        public static void IfNotEqual(object obja, object objb)
+        {
+            ifNotEqual(obja.GetType(), objb.GetType());
+        }
+        protected static void ifNotEqual(Type obja, Type objb)
+        {
+            if (obja != objb)
+                throw new WrongValidationTypeException();
+        }
+    }
+}
